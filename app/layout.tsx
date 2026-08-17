69d2d6194842fea747ec4950a10a41d86fd4c6b6
Innova Space <emorales@colprovidencia.cl>
2026-08-16 20:45:32 -0400

    Cargar navegación operativa responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import './compat.css';
 import './operations-extra.css';
 import './mobile-responsive.css';
+import './operations-mobile.css';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import 'leaflet/dist/leaflet.css';
 import ServiceWorkerRegister from '@/components/service-worker-register';
